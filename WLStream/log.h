66cef70f9e83d88c98661689c2a3b72fe093a79a
Václav Lipert <vaclavlipert@bglab.info>
2017-11-20 11:24:22 +0100

    Redirect errors to stderr so we can separate them from normal stream

--- WLStream/log.h
+++ WLStream/log.h
@@ -1,4 +1,4 @@
 // log.h
 
 #define LOG(format, ...) wprintf(format L"\n", __VA_ARGS__)
-#define ERR(format, ...) LOG(L"Error: " format, __VA_ARGS__)
+#define ERR(format, ...) fwprintf(stderr, format L"\n", __VA_ARGS__)
